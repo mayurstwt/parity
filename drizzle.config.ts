740c7d795b5acd253a35d15f5ef80dd7449c8e26
mayurstwt <mayurstwt@.com>
2025-01-09 21:05:27 +0530

    files added and db:migration done

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,12 +2,12 @@ import {env} from "@/app/data/env/server";
 import {defineConfig} from 'drizzle-kit';
 
 export default defineConfig({
-    schema: "./src/drizzle/schema.ts",
-    out: "./src/drizzle/migrations",
+    schema: "./app/drizzle/schema.ts",
+    out: "./app/drizzle/migrations",
     dialect: "postgresql",
     strict: true,
     verbose: true,
     dbCredentials: {
-        url: env.DATABASE_URL,
+      url: env.DATABASE_URL,
     },
-})
+  })
